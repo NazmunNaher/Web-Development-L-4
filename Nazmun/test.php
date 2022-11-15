9dec9826605fae3f92e2e0cb4bbc5fe4d9e2f300
NazmunNaher <115462094+NazmunNaher@users.noreply.github.com>
2022-11-15 17:20:12 +0600

    Add files via upload

---
+++ Nazmun/test.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="one">
+    Welcome
+<h1>DMPI</h1>
+<?php
+echo '<h1> Nazmun Nahar</h1>';
+echo '<h2>Roll-143598  </h2>';
+echo '<h3> Dept.Computer  </h3>';
+ 
+$num1=50;
+$num2=60;
+$result=$num1+$num2;
+echo $result;
+
+?>
+    </div>
+    <div class="two">
+        <h5>Menu</h5>
+    </div>
+   
+    
+</body>
+</html>
